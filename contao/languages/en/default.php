a92f9ef2651ce88574e383869779be7a35d828c9
Hendrik Bengs <hben@designerei-werbeagentur.de>
2026-01-12 15:47:47 +0100

    Added translations (english)

---
+++ contao/languages/en/default.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['layout'] = ['Layout', 'Creates a nested layout element (container, grid, flex).'];
---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,25 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['layout_legend'] = 'Layout settings';
+$GLOBALS['TL_LANG']['tl_content']['style_legend'] = 'Design settings';
+$GLOBALS['TL_LANG']['tl_content']['layout_list_legend'] = 'Layout settings (list)';
+$GLOBALS['TL_LANG']['tl_content']['spacing'] = ['Element spacing', 'Here you can define the spacing outside and inside the element.'];
+$GLOBALS['TL_LANG']['tl_content']['layoutType'] = ['Layout type', 'Here you can choose the layout type.'];
+$GLOBALS['TL_LANG']['tl_content']['gap'] = ['Row and column gap', 'Here you can control the spacing between rows and/or columns.'];
+$GLOBALS['TL_LANG']['tl_content']['flexDirection'] = ['Direction', 'Here you can define in which direction (horizontal, vertical) the child elements are aligned.'];
+$GLOBALS['TL_LANG']['tl_content']['flexWrap'] = ['Wrap', 'Here you can control the wrapping of the child elements.'];
+$GLOBALS['TL_LANG']['tl_content']['flex'] = ['Flex behavior', 'Here you can define the combined grow and shrink behavior of the element.'];
+$GLOBALS['TL_LANG']['tl_content']['gridTemplateColumns'] = ['Grid columns', 'Here you can define the number of grid columns.'];
+$GLOBALS['TL_LANG']['tl_content']['gridTemplateRows'] = ['Grid rows', 'Here you can define the number of grid rows.'];
+$GLOBALS['TL_LANG']['tl_content']['alignment'] = ['Alignment', 'Here you can control the alignment of the child elements.'];
+$GLOBALS['TL_LANG']['tl_content']['gridColumn'] = ['Columns (grid)', 'Here you can define how the elements are placed within the grid column.'];
+$GLOBALS['TL_LANG']['tl_content']['gridRow'] = ['Rows (grid)', 'Here you can define how the elements are placed within the grid row.'];
+$GLOBALS['TL_LANG']['tl_content']['order'] = ['Order', 'Here you can control the position of the element within the order.'];
+$GLOBALS['TL_LANG']['tl_content']['alignmentSelf'] = ['Alignment', 'Here you can control the alignment of the element.'];
+$GLOBALS['TL_LANG']['tl_content']['flexBasis'] = ['Width (flex)', 'Here you can define the width of the element.'];
+$GLOBALS['TL_LANG']['tl_content']['flexGrow'] = ['Grow behavior (flex)', 'Here you can define the grow behavior of the element.'];
+$GLOBALS['TL_LANG']['tl_content']['flexShrink'] = ['Shrink behavior (flex)', 'Here you can define the shrink behavior of the element.'];
+$GLOBALS['TL_LANG']['tl_content']['containerSize'] = ['Container width', 'Here you can set the maximum width of the container.'];
+$GLOBALS['TL_LANG']['tl_content']['containerCenter'] = ['Center container', 'Here you can define whether the container should be centered horizontally.'];
+$GLOBALS['TL_LANG']['tl_content']['columns'] = ['Columns', 'Here you can control the number/width of the child element columns.'];
+$GLOBALS['TL_LANG']['tl_content']['break'] = ['Break', 'Here you can define how a break should occur before, after, or within an element.'];
